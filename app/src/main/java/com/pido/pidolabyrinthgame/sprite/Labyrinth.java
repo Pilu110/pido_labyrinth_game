b8ae85d3e6441db6d74c2f560d5bddd0aad3c682
Istvan Vincze <v-v-ivincze@hotels.com>
2020-12-30 21:35:05 +0100

    generate better maze with branches

--- app/src/main/java/com/pido/pidolabyrinthgame/sprite/Labyrinth.java
+++ app/src/main/java/com/pido/pidolabyrinthgame/sprite/Labyrinth.java
@@ -4,11 +4,19 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.util.Log;
+import android.util.Pair;
 
 import com.pido.pidolabyrinthgame.R;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import static android.content.ContentValues.TAG;
 
 public class Labyrinth implements Sprite {
 
@@ -40,6 +48,9 @@ public class Labyrinth implements Sprite {
     public static final int NUM_OF_PLAYERS = 2;
 
     private final Tile[][] map;
+    private final int[][] depthMap;
+    private int maxDepth;
+
     private static final Map<Tile, Bitmap> tileImage = new HashMap<>();
     private static double scale;
     private static int scaledTileWidth, scaledTileHeight;
@@ -47,6 +58,7 @@ public class Labyrinth implements Sprite {
 
     public Labyrinth(Resources resources) {
         map = new Tile[MAP_WIDTH][MAP_HEIGHT];
+        depthMap = new int[MAP_WIDTH][MAP_HEIGHT];
 
         int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
         int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
@@ -69,11 +81,11 @@ public class Labyrinth implements Sprite {
     public void addSkulls() {
         int skulls = 0;
 
-        while (skulls < 10) {
+        while (skulls < 5) {
             int x = (int)(Math.random() * (MAP_WIDTH - 4)) + 3;
             int y = (int)(Math.random() * (MAP_HEIGHT - 4)) + 3;
 
-            if(map[x][y] == Tile.FLOOR) {
+            if(map[x][y] == Tile.FLOOR && depthMap[x][y] > maxDepth/2) {
                 map[x][y] = Tile.SKULL;
                 skulls++;
             }
@@ -83,17 +95,18 @@ public class Labyrinth implements Sprite {
 
     public void generateWalls() {
 
-        int[][] depthMap = new int[MAP_WIDTH][MAP_HEIGHT];
+        boolean [][] visited = new boolean[MAP_WIDTH][MAP_HEIGHT];
 
         for (int x = 0; x < MAP_WIDTH; x++) {
             for (int y = 0; y < MAP_HEIGHT; y++) {
                 map[x][y] = Tile.WALL;
                 depthMap[x][y] = Integer.MAX_VALUE;
+                visited[x][y] = false;
             }
         }
 
-        for (int x = 0; x < 2; x++) {
-            for (int y = 0; y < 2; y++) {
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
                 map[x][y] = Tile.FLOOR;
                 depthMap[x][y] = 0;
             }
@@ -102,112 +115,135 @@ public class Labyrinth implements Sprite {
 
         int iteration = 0;
 
-        int x = 1;
-        int y = 1;
-
         int numOfFloors = 9;
 
         double p1 = 0.4;
 
+        List<Point> branches = new ArrayList<>();
+
+        int x = 2;
+        int y = 2;
+
         do {
+            if (iteration % 10 == 0) {
+                branches.add(new Point(x , y));
+            }
 
-            Direction selectedDirection = Direction.NONE;
-            int maxDepth = 0;
+            for(Point branch : branches) {
 
-            for(Direction direction : Direction.values()) {
-                switch (direction) {
-                    case UP:
-                        if (y > 1 && x < MAP_WIDTH - 1 && x > 0
-                                && ((map[x][y - 1] == Tile.WALL
+                x = branch.x;
+                y = branch.y;
+
+                Direction selectedDirection = Direction.NONE;
+                int maxDepthNearby = 0;
+
+                for (Direction direction : Direction.values()) {
+                    switch (direction) {
+                        case UP:
+                            if (y > 1 && x < MAP_WIDTH - 1 && x > 0
+                                    && ((map[x][y - 1] == Tile.WALL
                                     && map[x - 1][y - 1] == Tile.WALL
                                     && map[x + 1][y - 1] == Tile.WALL
                                     && map[x][y - 2] == Tile.WALL)
                                     || map[x][y - 1] == Tile.FLOOR
-                                )
-                        ) {
-                            if(depthMap[x][y-1] > maxDepth || (depthMap[x][y-1] == maxDepth && Math.random() < p1)) {
-                                maxDepth = depthMap[x][y-1];
-                                selectedDirection = Direction.UP;
+                            )
+                            ) {
+                                if (!visited[x][y - 1] && depthMap[x][y - 1] > maxDepthNearby || (depthMap[x][y - 1] == maxDepthNearby && Math.random() < p1)) {
+                                    maxDepthNearby = depthMap[x][y - 1];
+                                    selectedDirection = Direction.UP;
+                                }
                             }
-                        }
-                        break;
-                    case RIGHT:
-                        if (x < MAP_WIDTH - 2 && y < MAP_HEIGHT - 1 && y > 0
-                                && ((map[x + 1][y] == Tile.WALL
+                            break;
+                        case RIGHT:
+                            if (x < MAP_WIDTH - 2 && y < MAP_HEIGHT - 1 && y > 0
+                                    && ((map[x + 1][y] == Tile.WALL
                                     && map[x + 1][y - 1] == Tile.WALL
                                     && map[x + 1][y + 1] == Tile.WALL
                                     && map[x + 2][y] == Tile.WALL)
                                     || map[x + 1][y] == Tile.FLOOR
-                                )
-                        ) {
-                            if(depthMap[x+1][y] > maxDepth || (depthMap[x+1][y] == maxDepth && Math.random() < p1)) {
-                                maxDepth = depthMap[x+1][y];
-                                selectedDirection = Direction.RIGHT;
+                            )
+                            ) {
+                                if (!visited[x + 1][y] && depthMap[x + 1][y] > maxDepthNearby || (depthMap[x + 1][y] == maxDepthNearby && Math.random() < p1)) {
+                                    maxDepthNearby = depthMap[x + 1][y];
+                                    selectedDirection = Direction.RIGHT;
+                                }
                             }
-                        }
-                        break;
-                    case DOWN:
-                        if (y < MAP_HEIGHT - 2 && x < MAP_WIDTH - 1 && x > 0
-                                && ((map[x][y + 1] == Tile.WALL
+                            break;
+                        case DOWN:
+                            if (y < MAP_HEIGHT - 2 && x < MAP_WIDTH - 1 && x > 0
+                                    && ((map[x][y + 1] == Tile.WALL
                                     && map[x - 1][y + 1] == Tile.WALL
                                     && map[x + 1][y + 1] == Tile.WALL
                                     && map[x][y + 2] == Tile.WALL)
-                                || map[x][y + 1] == Tile.FLOOR
+                                    || map[x][y + 1] == Tile.FLOOR
                             )
-                        ) {
-                            if(depthMap[x][y+1] > maxDepth || (depthMap[x][y+1] == maxDepth && Math.random() < p1)) {
-                                maxDepth = depthMap[x][y+1];
-                                selectedDirection = Direction.DOWN;
+                            ) {
+                                if (!visited[x][y + 1] && depthMap[x][y + 1] > maxDepthNearby || (depthMap[x][y + 1] == maxDepthNearby && Math.random() < p1)) {
+                                    maxDepthNearby = depthMap[x][y + 1];
+                                    selectedDirection = Direction.DOWN;
+                                }
                             }
-                        }
-                        break;
+                            break;
 
-                    case LEFT:
-                        if (x > 1 && y < MAP_HEIGHT -1 && y > 0
-                                && ((map[x - 1][y] == Tile.WALL
+                        case LEFT:
+                            if (x > 1 && y < MAP_HEIGHT - 1 && y > 0
+                                    && ((map[x - 1][y] == Tile.WALL
                                     && map[x - 1][y - 1] == Tile.WALL
                                     && map[x - 1][y + 1] == Tile.WALL
                                     && map[x - 2][y] == Tile.WALL)
-                                || map[x - 1][y] == Tile.FLOOR
+                                    || map[x - 1][y] == Tile.FLOOR
                             )
-                        ) {
-                            if(depthMap[x-1][y] > maxDepth || (depthMap[x-1][y] == maxDepth && Math.random() < p1)) {
-                                maxDepth = depthMap[x-1][y];
-                                selectedDirection = Direction.LEFT;
+                            ) {
+                                if (!visited[x - 1][y] && depthMap[x - 1][y] > maxDepthNearby || (depthMap[x - 1][y] == maxDepthNearby && Math.random() < p1)) {
+                                    maxDepthNearby = depthMap[x - 1][y];
+                                    selectedDirection = Direction.LEFT;
+                                }
                             }
+                            break;
                         }
+                    }
+
+                    if (selectedDirection == Direction.NONE) {
                         break;
-                }
+                    }
+
+                    int previousDepth = depthMap[x][y];
+
+                    int previousX = x;
+                    int previousY = y;
+
+                    switch (selectedDirection) {
+                        case UP:
+                            y--;
+                            break;
+                        case RIGHT:
+                            x++;
+                            break;
+                        case DOWN:
+                            y++;
+                            break;
+                        case LEFT:
+                            x--;
+                            break;
+                    }
+
+                    if (depthMap[x][y] == Integer.MAX_VALUE) {
+                        depthMap[x][y] = previousDepth + 1;
+
+                        if (depthMap[x][y] > maxDepth) {
+                            maxDepth = depthMap[x][y];
+                        }
+                    } else if (map[x][y] == Tile.FLOOR) {
+                        visited[previousX][previousY] = true;
+                    }
+
+                    map[x][y] = Tile.FLOOR;
+
+                    branch.set(x, y);
             }
-
-            if(selectedDirection == Direction.NONE) {
-                break;
-            }
-
-            int previousDepth = depthMap[x][y];
-
-            int previousX = x;
-            int previousY = y;
-
-            switch (selectedDirection) {
-                case UP: y--; break;
-                case RIGHT: x++; break;
-                case DOWN: y++; break;
-                case LEFT: x--; break;
-            }
-
-            if(depthMap[x][y] == Integer.MAX_VALUE) {
-                depthMap[x][y] = previousDepth + 1;
-            }
-            else if(map[x][y] == Tile.FLOOR) {
-                depthMap[previousX][previousY] = 0;
-            }
-
-            map[x][y] = Tile.FLOOR;
-
 
             iteration++;
-        } while (numOfFloors < MAP_HEIGHT * MAP_WIDTH / 3 && iteration < 5000);
+        } while (numOfFloors < MAP_HEIGHT * MAP_WIDTH / 3 && iteration < 10000);
     }
 
     @Override
